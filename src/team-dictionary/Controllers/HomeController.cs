2f08562e2c706e58a78c1ead73181a84e2d6f4ee
Anton Miroshkin <anton.miroshkin@gmail.com>
2018-09-19 18:39:51 +0300

    Add search button

--- src/team-dictionary/Controllers/HomeController.cs
+++ src/team-dictionary/Controllers/HomeController.cs
@@ -15,23 +15,14 @@ namespace team_dictionary.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Search()
+        {
+            return View();
         }
     }
 }
