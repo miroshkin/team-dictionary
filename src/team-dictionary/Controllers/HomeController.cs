0530367f8fd122597c4128afa7f54d1904399f14
Anton Miroshkin <anton.miroshkin@gmail.com>
2018-09-24 18:17:12 +0300

    Add ignore case search

--- src/team-dictionary/Controllers/HomeController.cs
+++ src/team-dictionary/Controllers/HomeController.cs
@@ -35,8 +35,11 @@ namespace team_dictionary.Controllers
             }
             else
             {
-                model.SearchResult = _wordsRepository.GetWords()
-                    .Where(c => c.Name.Contains(searchString) | c.Translation.Contains(searchString));
+                model.SearchResult = 
+                    _wordsRepository.GetWords()
+                    .Where(c => 
+                         c.Name.ToUpper().Contains(searchString.ToUpper()) |
+                         c.Translation.ToUpper().Contains(searchString.ToUpper()));
             }
 
             return View(model);
