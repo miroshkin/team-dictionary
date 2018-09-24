cc41b201d9779bc62d4a429f292f6d8bdb940760
Anton Miroshkin <anton.miroshkin@gmail.com>
2018-09-24 18:54:21 +0300

    Add new words and theme

--- src/team-dictionary/Data/WordsRepository.cs
+++ src/team-dictionary/Data/WordsRepository.cs
@@ -17,6 +17,32 @@ namespace team_dictionary.Data
             entries.Add(new Word("проводка", "provodka"));
             entries.Add(new Word("списание", "decommission"));
             entries.Add(new Word("сличительная ведомость", "collation statement"));
+            entries.Add(new Word("счет", "account"));
+            entries.Add(new Word("детализация", "details"));
+            entries.Add(new Word("документ", "document"));
+            entries.Add(new Word("платеж", "payment"));
+            entries.Add(new Word("сверка", "sverka"));
+            entries.Add(new Word("требование", "trebovanie"));
+            entries.Add(new Word("возврат", "return"));
+            entries.Add(new Word("расписание", "schedule"));
+            entries.Add(new Word("склад", "warehouse"));
+            entries.Add(new Word("жалоба", "complaint"));
+            entries.Add(new Word("вес", "weight"));
+            entries.Add(new Word("объем", "volume"));
+            entries.Add(new Word("страна", "country"));
+            entries.Add(new Word("отчет", "report"));
+            entries.Add(new Word("план", "plan"));
+            entries.Add(new Word("транспортная компания", "transport company"));
+            entries.Add(new Word("безопасность", "security"));
+            entries.Add(new Word("контрафакт", "contrafact"));
+            entries.Add(new Word("контрафакт", "counterfeit"));
+            entries.Add(new Word("инвойс", "invoice"));
+            entries.Add(new Word("таможня", "customs"));
+            entries.Add(new Word("договор", "контракты"));
+
+
+
+
             return entries;
 
         }
