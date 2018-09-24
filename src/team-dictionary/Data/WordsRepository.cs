010b74836ccdf99a7f6c657c4ab2d554d9a4bf91
Anton Miroshkin <anton.miroshkin@gmail.com>
2018-09-24 15:49:38 +0300

    Add dictionary json.

--- src/team-dictionary/Data/WordsRepository.cs
+++ src/team-dictionary/Data/WordsRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace team_dictionary.Data
 {
     public class WordsRepository
@@ -12,9 +13,12 @@ namespace team_dictionary.Data
             var entries = new List<Word>();
             entries.Add(new Word("поставщик", "supplier"));
             entries.Add(new Word("клиент", "customer"));
-            entries.Add(new Word("контагент", "contragent"));
+            entries.Add(new Word("контрагент", "contragent"));
+            entries.Add(new Word("проводка", "provodka"));
+            entries.Add(new Word("списание", "decommission"));
 
             return entries;
+
         }
     }
 }
