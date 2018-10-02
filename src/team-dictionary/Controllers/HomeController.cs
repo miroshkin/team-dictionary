fca79bc11b8ebbda75b6998b644fe84a6da56ada
Anton Miroshkin <anton.miroshkin@gmail.com>
2018-10-02 14:15:24 +0300

    Added json dictionary file

--- src/team-dictionary/Controllers/HomeController.cs
+++ src/team-dictionary/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using team_dictionary.Data;
 using team_dictionary.Models;
@@ -12,15 +15,21 @@ namespace team_dictionary.Controllers
     public class HomeController : Controller
     {
         private WordsRepository _wordsRepository = null;
-
-        public HomeController()
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public HomeController(IHostingEnvironment hostingEnvironment)
         {
-            _wordsRepository = new WordsRepository();
+            _hostingEnvironment = hostingEnvironment;
+            string contentRootPath = _hostingEnvironment.ContentRootPath;
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var enc1251 = Encoding.GetEncoding(1251);
+            var JSON = System.IO.File.ReadAllText(contentRootPath + "/Content/dictionary.json", enc1251);
+            _wordsRepository = new WordsRepository(JSON);
         }
 
         public IActionResult Index()
         {
             SearchModel sm = new SearchModel();
+           
             return View();
         }
 
@@ -29,9 +38,11 @@ namespace team_dictionary.Controllers
         {
             var searchString = model.SearchString;
 
+            
+
             if (String.IsNullOrEmpty(model.SearchString))
             {
-                model.SearchResult = new Word[0];
+                model.SearchResult = _wordsRepository.GetWords();
             }
             else
             {
